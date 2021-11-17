725bc7be8a392f96e0114e2402ef98d887b92695
matias virgili <matiasvirgili@gmail.com>
2021-11-17 19:13:06 -0300

    setting proyect

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+export const Header = ()=>{
+    return (
+        <div className={styles.header}>
+            <h1>Users App</h1>
+        </div>
+    )
+}
+
---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import UsersApp from './UsersApp'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <UsersApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
