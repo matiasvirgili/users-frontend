ab06d609a33855bed7a9954a0b3224dba656d4f1
Matias Virgili <matiasvirgili@gmail.com>
2021-11-23 16:34:31 -0300

    users reducer finished

---
+++ src/components/users/User.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FaTimes as DeleteIcon } from 'react-icons/fa';
+import { MdEdit as EditIcon } from 'react-icons/md';
+import styles from './User.module.css';
+import { useDispatch } from 'react-redux';
+import { deleteUserAsync } from '../../redux/actions/usersAction';
+
+export const User = ({ user, onModify }) => {
+  const { id, name, lastName, telephone, direction, dni } = user;
+  const dispatch = useDispatch();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.column}>
+        <span className={styles.title}>Full name</span>
+        <span className={styles.content}>{name + ' ' + lastName}</span>
+      </div>
+      <div className={styles.column}>
+        <span className={styles.title}>Telephone</span>
+        <span className={styles.content}>{telephone}</span>
+      </div>
+      <div className={styles.column}>
+        <span className={styles.title}>Direction</span>
+        <span className={styles.content}>{direction}</span>
+      </div>
+      <div className={styles.column}>
+        <span className={styles.title}>DNI</span>
+        <span className={styles.content}>{dni}</span>
+      </div>
+      <div className={styles.actions}>
+        <EditIcon className={styles.editIcon} onClick={() => onModify(id)} />
+        <DeleteIcon
+          className={styles.deleteIcon}
+          onClick={() => dispatch(deleteUserAsync(id))}
+        />
+      </div>
+    </div>
+  );
+};
---
+++ src/redux/reducers/usersReducer.js
@@ -0,0 +1,63 @@
+import {
+  CREATE_USER,
+  UPDATE_USER,
+  DELETE_USER,
+  SET_ALL_USERS,
+  SET_ERROR,
+  SET_LOADING_TRUE,
+} from '../types/usersTypes';
+
+const initialState = {
+  list: [],
+  error: '',
+  isLoading: false,
+};
+
+export const usersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CREATE_USER:
+      return {
+        ...state,
+        error: '',
+        list: [...state.list, action.payload],
+        isLoading: false,
+      };
+    case UPDATE_USER:
+      return {
+        ...state,
+        error: '',
+        list: state.list.map((user) =>
+          user.id === action.payload.id ? action.payload : user
+        ),
+        isLoading: false,
+      };
+    case DELETE_USER:
+      return {
+        ...state,
+        error: '',
+        list: state.list.filter((user) => user.id !== action.payload),
+        isLoading: false,
+      };
+    case SET_ALL_USERS:
+      return {
+        ...state,
+        error: '',
+        list: action.payload,
+        isLoading: false,
+      };
+    case SET_ERROR:
+      return {
+        ...state,
+        error: action.payload || 'An error ocurred',
+        isLoading: false,
+      };
+    case SET_LOADING_TRUE:
+      return {
+        ...state,
+        isLoading: true,
+      };
+
+    default:
+      return state;
+  }
+};
